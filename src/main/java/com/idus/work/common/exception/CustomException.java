2c675d3b28883ae9be980cbc035a9b344cf5a295
박정진 <justdoit@bithumbsystems.com>
2022-09-29 14:10:10 +0900

    custom exception 생성

--- src/main/java/com/idus/work/common/exception/CustomException.java
+++ src/main/java/com/idus/work/common/exception/CustomException.java
@@ -1,11 +1,14 @@
 package com.idus.work.common.exception;
 
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.lang.module.FindException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,10 +16,18 @@ import java.util.Map;
 public class CustomException {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex){
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors()
                 .forEach(c -> errors.put(((FieldError) c).getField(), c.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler({DuplicateKeyException.class, FindException.class})
+    public ResponseEntity<Map<String, String>> handleDuplicateKeyException(Exception ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("status", String.valueOf(HttpStatus.BAD_REQUEST));
+        errors.put("message", ex.getMessage());
         return ResponseEntity.badRequest().body(errors);
     }
 }
