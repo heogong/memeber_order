181058ebd06dedad38fb087e9e11cc7c4e40be03
박정진 <justdoit@bithumbsystems.com>
2022-09-29 14:11:21 +0900

    애플리케이션 구동시 초기 order 등록

---
+++ src/main/java/com/idus/work/order/InitOrderApplicationRunner.java
@@ -0,0 +1,23 @@
+package com.idus.work.order;
+
+import com.idus.work.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class InitOrderApplicationRunner implements ApplicationRunner {
+    private final OrderService orderService;
+
+    /**
+     * 애플리케이션 구동 시 초기 주문정보 세잍
+     * @GeneratedValue 커스텀으로 인해 import.sql insert가 안됨
+     *
+     */
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        orderService.initOrderData();
+    }
+}
