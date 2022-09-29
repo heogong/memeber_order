5b01e0c731f99cae035f08effb71106a0b6e86fe
박정진 <justdoit@bithumbsystems.com>
2022-09-29 14:45:26 +0900

    초기 비밀번호 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO member (name, nick_name, email, phone_number, gender) VALUES('자바', '스프링', 'spring@boot.com', 010, 'MALE');
-INSERT INTO member (name, nick_name, email, phone_number, gender) VALUES('코틀린', '코루틴', 'kotlin@corouine.com', 020, 'FEMALE');
-INSERT INTO member (name, nick_name, email, phone_number, gender) VALUES('파이썬', 'fastapi', 'python@fastapi.com', 030, 'FEMALE');
-INSERT INTO member (name, nick_name, email, phone_number, gender) VALUES('자바스크립트', '리액트', 'script@react.com', 040, 'MALE');
-INSERT INTO member (name, nick_name, email, phone_number, gender) VALUES('자바스크립트2', '뷰뷰', 'script2@vue.com', 050, 'MALE');
-INSERT INTO member (name, nick_name, email, phone_number, gender) VALUES('db', 'mysql', 'db@mysql.com', 060, 'MALE');
+INSERT INTO member (name, nick_name, email, phone_number, gender, password) VALUES('자바', '스프링', 'spring@boot.com', 010, 'MALE', '$2a$10$6GRS4JnFMGns67/TL70X7ugMUiI/LJ4mcJed7IGEkSxKiLDWcnSPG');
+INSERT INTO member (name, nick_name, email, phone_number, gender, password) VALUES('코틀린', '코루틴', 'kotlin@corouine.com', 020, 'FEMALE', '$2a$10$6GRS4JnFMGns67/TL70X7ugMUiI/LJ4mcJed7IGEkSxKiLDWcnSPG');
+INSERT INTO member (name, nick_name, email, phone_number, gender, password) VALUES('파이썬', 'fastapi', 'python@fastapi.com', 030, 'FEMALE', '$2a$10$6GRS4JnFMGns67/TL70X7ugMUiI/LJ4mcJed7IGEkSxKiLDWcnSPG');
+INSERT INTO member (name, nick_name, email, phone_number, gender, password) VALUES('자바스크립트', '리액트', 'script@react.com', 040, 'MALE', '$2a$10$6GRS4JnFMGns67/TL70X7ugMUiI/LJ4mcJed7IGEkSxKiLDWcnSPG');
+INSERT INTO member (name, nick_name, email, phone_number, gender, password) VALUES('자바스크립트2', '뷰뷰', 'script2@vue.com', 050, 'MALE', '$2a$10$6GRS4JnFMGns67/TL70X7ugMUiI/LJ4mcJed7IGEkSxKiLDWcnSPG');
+INSERT INTO member (name, nick_name, email, phone_number, gender, password) VALUES('db', 'mysql', 'db@mysql.com', 060, 'MALE', '$2a$10$6GRS4JnFMGns67/TL70X7ugMUiI/LJ4mcJed7IGEkSxKiLDWcnSPG');
