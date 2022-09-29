ea7538e27b9c0c87eac13330e62c6952795e2abf
박정진 <justdoit@bithumbsystems.com>
2022-09-29 17:43:36 +0900

    테스트용 시큐리티 config

--- src/main/java/com/idus/work/core/config/security/SecurityConfig.java
+++ src/main/java/com/idus/work/core/config/security/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.idus.work.core.config.security;
 
+import com.idus.work.member.service.CustomUserDetailService;
 import com.idus.work.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -16,27 +19,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    private final MemberService memberService;
+    private final CustomUserDetailService customUserDetailService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
                 .csrf().disable()
+//                .httpBasic().and()
                 .authorizeHttpRequests()
-                .antMatchers("/member").hasRole("USER")
+//                .antMatchers("/member").hasRole("USER")
                 .antMatchers("/", "/**").permitAll()
+//                .antMatchers("/", "/**").hasRole("USER")
                 .anyRequest().authenticated()
-//                .permitAll()
                 .and()
-                .userDetailsService(memberService)
-
-
+                .userDetailsService(customUserDetailService)
         ;
         return http.build();
     }
-
-
 
     @Bean
     public WebSecurityCustomizer configure() {
