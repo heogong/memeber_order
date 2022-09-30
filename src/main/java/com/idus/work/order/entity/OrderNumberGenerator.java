ef7ef9a13179e133bdfa15bc57a6597fc9bfe710
박정진 <justdoit@bithumbsystems.com>
2022-09-30 16:42:18 +0900

    줄 제거

--- src/main/java/com/idus/work/order/entity/OrderNumberGenerator.java
+++ src/main/java/com/idus/work/order/entity/OrderNumberGenerator.java
@@ -14,7 +14,6 @@ public class OrderNumberGenerator implements IdentifierGenerator {
         int rightLimit = 122; // letter 'z'
         int targetStringLength = 12;
         Random random = new Random();
-
         return random.ints(leftLimit, rightLimit + 1)
                 .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
                 .limit(targetStringLength)
