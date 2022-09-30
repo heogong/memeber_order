5a599a89c985b526faac340606e3f1c1e3a29ce0
박정진 <justdoit@bithumbsystems.com>
2022-09-30 10:01:16 +0900

    Gender 등록 시 유효하지 않은 값 처리

--- src/main/java/com/idus/work/common/constant/Gender.java
+++ src/main/java/com/idus/work/common/constant/Gender.java
@@ -1,5 +1,18 @@
 package com.idus.work.common.constant;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public enum Gender {
-    NONE, MALE, FEMALE
+    NONE, MALE, FEMALE;
+
+    @JsonCreator
+    public static Gender create(String requestValue) {
+        return Stream.of(values())
+                .filter(gender -> gender.toString().equalsIgnoreCase(requestValue))
+                .findFirst()
+                .orElse(NONE);
+    }
 }
