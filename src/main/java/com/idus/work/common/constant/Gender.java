befabbc71cecd18304b215c7dd2b5c31d300a964
박정진 <justdoit@bithumbsystems.com>
2022-09-30 16:40:09 +0900

    value 추가

--- src/main/java/com/idus/work/common/constant/Gender.java
+++ src/main/java/com/idus/work/common/constant/Gender.java
@@ -1,12 +1,19 @@
 package com.idus.work.common.constant;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.stream.Stream;
 
+@Getter
 public enum Gender {
-    NONE, MALE, FEMALE;
+    NONE("미선택"), MALE("남성"), FEMALE("여성");
+
+    final String korGender;
+    Gender(String korGender){
+        this.korGender = korGender;
+    }
 
     @JsonCreator
     public static Gender create(String requestValue) {
